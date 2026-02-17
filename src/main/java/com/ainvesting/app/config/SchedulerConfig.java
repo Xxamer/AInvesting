9f6601146c4a4d3fcbf1677e443628fc04576035
Christian Granado <xxamer_@hotmail.com>
2026-02-17 22:18:28 +0000

    Added scheduler configuration

--- src/main/java/com/ainvesting/app/config/SchedulerConfig.java
+++ src/main/java/com/ainvesting/app/config/SchedulerConfig.java
@@ -1,4 +1,20 @@
 package com.ainvesting.app.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
 public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("AInvesting-Scheduler-");
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setAwaitTerminationSeconds(60);
+        return scheduler;
+
+    }
 }
